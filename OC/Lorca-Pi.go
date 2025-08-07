894961f451010a79cc5570efb33b0b1f80e2f7e4
2220047-Omura <h20047@e.hiroshima-cu.ac.jp>
2025-08-08 00:30:01 +0900

    add file

--- OC/Lorca-Pi.go
+++ OC/Lorca-Pi.go
@@ -2,175 +2,28 @@ package main
 
 import (
 	"fmt"
+	"math"
+	"math/big"
+	"math/rand/v2"
 	"net/url"
 	"strconv"
+	"strings"
 	"time"
 
 	"github.com/zserge/lorca"
 )
 
 var ui lorca.UI
-var l, N, v2 int
 var str string
-var contFib bool
 
-type fib struct {
-	number      int
-	level       int
-	N           int
-	calc        bool
-	append      bool
-	left, right *fib
-}
-
-type lv struct {
-	level  int
-	number []int
-	next   *lv
-}
-
-func (f *fib) fibTree(n int) *fib {
-	N += 1
-	if f == nil {
-		if n != 1 && n != 0 {
-			f = &fib{number: n, level: l, N: N}
-		} else {
-			f = &fib{number: 1, level: l, N: N, calc: true}
-		}
-		if n > 1 {
-			l = f.level + 1
-			f.left = f.left.fibTree(n - 1)
-			l = f.level + 1
-			f.right = f.right.fibTree(n - 2)
-		}
-	}
-	return f
-}
-
-func (f *fib) nodeCalc() *fib {
-	contFib = false
-	if f.left != nil && f.right != nil {
-		if f.left.calc == true && f.right.calc == true {
-			contFib = true
-			f.number = f.left.number + f.right.number
-			f.calc = true
-		}
-	}
-	if f.left != nil {
-		if f.left.calc == false {
-			f.left = f.left.nodeCalc()
-		}
-	}
-	if f.right != nil {
-		if f.right.calc == false {
-			f.right = f.right.nodeCalc()
-		}
-	}
-	return f
-}
-
-func (f *fib) printTree() {
-	fmt.Println(f)
-	if f.left != nil {
-		f.left.printTree()
-	}
-	if f.right != nil {
-		f.right.printTree()
-	}
-}
-
-func (lv1 *lv) setLv(v int) *lv {
-	if lv1 == nil {
-		lv1 = &lv{level: v2}
-		v2 += 1
-		if v2 < v {
-			lv1.next = lv1.next.setLv(v)
-		}
-	}
-	return lv1
-}
-
-func (f *fib) appendLv(lv *lv) {
-	if f.level == lv.level && f.append == false {
-		lv.number = append(lv.number, f.number)
-		f.append = true
-	} else {
-		if lv.next != nil {
-			f.appendLv(lv.next)
-		}
-	}
-	if f.left != nil {
-		f.left.appendLv(lv)
-	}
-	if f.right != nil {
-		f.right.appendLv(lv)
-	}
-}
-
-func (lv *lv) printLv() {
-	fmt.Println(lv)
-	if lv.next != nil {
-		lv.next.printLv()
-	}
-}
-
-func (lv *lv) lvToStr() {
-	str += "<center>"
-	for _, v := range lv.number {
-		a := strconv.Itoa(v)
-		str += (a + " ")
-	}
-	str += ("</center>" + "<br/>")
-	if lv.next != nil {
-		lv.next.lvToStr()
-	} else {
-		//V = str
-		return
-	}
-}
-
-func (f *fib) search(n int, c1 chan int) {
-	/*
-		if l == f.level && n == f.number {
-			c1 <- f.number
-		}
-	*/
-	if n == f.N {
-		c1 <- f.number
-	}
-
-	if f.left != nil {
-		f.left.search(n, c1)
-	}
-	if f.right != nil {
-		f.right.search(n, c1)
-	}
-}
-
-func (f *fib) searchGo(n int, c2 chan int) {
-	/*
-		if l == f.level && n == f.number {
-			c2 <- f.number
-		}
-	*/
-	if n == f.N {
-		c2 <- f.number
-	}
-	if f.left != nil {
-		go f.left.searchGo(n, c2)
-	}
-	if f.right != nil {
-		go f.right.searchGo(n, c2)
-	}
-}
-
+// "--remote-allow-origins=*"
 func main() {
 	ui, _ = lorca.New("", "", 1200, 900, "--remote-allow-origins=*")
 	defer ui.Close()
 
 	// GoのfuncをJavaScriptで呼び出せるようBind
-	ui.Bind("fibTree", fibTree)
-	ui.Bind("searchTree", searchTree)
+	ui.Bind("test", test)
+	ui.Bind("reset", reset)
 
 	// 今回はurl.PathEscapeでそのままHTMLを配置
 	// ※下記はHTTPサーバをたててそこから取得する例
@@ -183,27 +36,19 @@ func main() {
     <head>
 
 	<script>
-		function clickBtn3() {
-    		const number2 = document.getElementById("number2");
-			document.getElementById("span2").textContent = number2.value;
-			return number2.value
-		}
-		function clickSearch() {
-			const number3 = document.getElementById("number3");
-			return number3.value
+		function clickBtn() {
+    		const plot = document.getElementById("plot");
+			const thread = document.getElementById("thread");
+			return plot.value+","+thread.value
 		}
     </script>
 	</head>
     <body>
-	<p>terms : <span id="span2"></span></p>
-	<input type="number" id="number2" value="4" max="2000" />
-	<input type="button" value="make" onclick="fibTree()" />
-	<br/>
-	number : <input type="number" id="number3" value="0" max="2000" />
-	<input type="button" value="search" onclick="searchTree()" />
-        <div id="content3"></div>
-		<br/>
-		<div id="content2"></div>
+	Plot : <input type="number" id="plot" value="1000000" max="1000000000" /><br/>
+	Thread : <input type="number" id="thread" value="1" max="2000" /><br/>
+	<input type="button" value="Test" onclick="test()" />
+	<input type="button" value="Reset" onclick="reset()" />
+    <div id="content"></div>
     </body>
     </html>
 	`))
@@ -211,73 +56,175 @@ func main() {
 	<-ui.Done()
 }
 
-//class="fit-picture"
 /*
-crossorigin=anonymous
-		referrerpolicy=no-referrer
-		<img
-		src="/Users/omura/OC/img/mc2.png"
-		alt="mc-pic"
-		width="1169"
-		height="826"
-		 />
+<input type="button" value="Seq Float" onclick="SeqFloat()" />
+	<input type="button" value="Go Float" onclick="GoFloat()" />
+	<input type="button" value="Seq Big" onclick="SeqBig()" />
+	<input type="button" value="Go Big" onclick="GoBig()" />
 */
 
-var f *fib
-var Lv *lv
+func test() {
+	var Pi1, Pi1Sum, Pi2, Pi2Sum, Pi3, Pi3Sum, Pi4, Pi4Sum int
+	var str2 string
+	str += "<tr>"
 
-func fibTree() {
+	input := ui.Eval(`clickBtn();`).String()
+	Split := strings.Split(input, ",")
+	Tstr := Split[1]
+	plot, _ := strconv.Atoi(Split[0])
+	thread, _ := strconv.Atoi(Split[1])
+	n := plot / thread
 
-	//var str2 string
-	// JavaScriptのfunction呼び出し
-	str1 := ui.Eval(`clickBtn3();`).String()
-	v, _ := strconv.Atoi(str1)
-	// HTMLに反映
-	f = f.fibTree(v)
-	for {
-		f = f.nodeCalc()
-		if contFib == false {
-			break
-		}
+	//fmt.Println("MCFloat")
+	c1 := make(chan int, thread)
+	t1 := time.Now()
+	for i := 0; i < thread; i++ {
+		MCFloat(n, c1)
 	}
-	f.printTree()
-	Lv = Lv.setLv(v)
+	for i := 0; i < thread; i++ {
+		Pi1 = <-c1
+		Pi1Sum += Pi1
+	}
+	t2 := time.Now()
+	var Ans1 float64 = (float64(Pi1Sum) / float64(plot)) * 4
+	//fmt.Println(Ans1)
+	//str += ("<td> Seq Float : " + t2.Sub(t1).String() + "</td>")
+	str += ("<td>" + t2.Sub(t1).String() + "</td>")
+	fmt.Println("MCFloat Ans: ", Ans1, " Time: ", t2.Sub(t1), "\n")
 
-	f.appendLv(Lv)
-	Lv.printLv()
+	//fmt.Println("MCgoFloat")
+	c2 := make(chan int, thread)
+	t3 := time.Now()
+	for i := 0; i < thread; i++ {
+		go MCgoFloat(n, c2)
+	}
+	for i := 0; i < thread; i++ {
+		Pi2 = <-c2
+		Pi2Sum += Pi2
+	}
+	t4 := time.Now()
+	var Ans2 float64 = (float64(Pi2Sum) / float64(plot)) * 4
 
-	Lv.lvToStr()
-	//fmt.Println(str2)
-	ui.Eval(`document.getElementById('content2').innerHTML="` + str + `";`)
+	t24 := strconv.FormatFloat(float64(t2.Sub(t1))/float64(t4.Sub(t3)), 'f', 2, 64)
+	//fmt.Print(Ans3)
+	//str += ("<td>&ensp; Go Float : " + t4.Sub(t3).String() + "</td>")
+	str += ("<td>" + t4.Sub(t3).String() + " (th=" + Tstr + ", x" + string(t24) + ")</td>")
+	fmt.Println("MCgoFloat Ans: ", Ans2, " Time: ", t4.Sub(t3), "\n")
 
-	l, v2 = 0, 0
-	str=""
+	//fmt.Println("MCBig")
+	c3 := make(chan int, thread)
+	t5 := time.Now()
+	for i := 0; i < thread; i++ {
+		MCBig(n, c3)
+	}
+	for i := 0; i < thread; i++ {
+		Pi3 = <-c3
+		Pi3Sum += Pi3
+	}
+	t6 := time.Now()
+	var Ans3 float64 = (float64(Pi3Sum) / float64(plot)) * 4
+	//fmt.Println(Ans2)
+	//str += ("<td>&ensp; Seq Big : " + t6.Sub(t5).String() + "</td>")
+	str += ("<td>" + t6.Sub(t5).String() + "</td>")
+	fmt.Println("MCBig Ans: ", Ans3, " Time: ", t5.Sub(t6), "\n")
+
+	//fmt.Println("MCgoBig")
+	c4 := make(chan int, thread)
+	t7 := time.Now()
+	for i := 0; i < thread; i++ {
+		go MCgoBig(n, c4)
+	}
+	for i := 0; i < thread; i++ {
+		Pi4 = <-c4
+		Pi4Sum += Pi4
+	}
+	t8 := time.Now()
+	var Ans4 float64 = (float64(Pi4Sum) / float64(plot)) * 4
+
+	t68 := strconv.FormatFloat(float64(t6.Sub(t5))/float64(t8.Sub(t7)), 'f', 2, 64)
+	//fmt.Println(Ans2)a
+	//str += ("<td>&ensp; Go Big : " + t8.Sub(t7).String() + "</td>")
+	str += ("<td>" + t8.Sub(t7).String() + " (th=" + Tstr + ", x" + string(t68) + ")</td>")
+	fmt.Println("MCgoBig Ans: ", Ans4, " Time: ", t8.Sub(t7), "\n")
+
+	str += "</tr>"
+	str2 = "<table>" + "<tr><td>|------- Float (Seq) -------|</td><td>|------- Float (Goroutine) -------|</td><td>|------- Big (Seq) -------|</td><td>|------- Big (Goroutine) -------|</td></tr>" + str + "</table>"
+	ui.Eval(`document.getElementById('content').innerHTML="` + str2 + `";`)
 }
 
-func searchTree() {
-	str2 := ui.Eval(`clickSearch();`).String()
-	/*
-		Split := strings.Split(str2, ",")
-		level, _ := strconv.Atoi(Split[0])
-		number, _ := strconv.Atoi(Split[1])
-	*/
-	number, _ := strconv.Atoi(str2)
-	c1 := make(chan int, 1)
-	c2 := make(chan int, 1)
+func reset() {
+	str = ""
+	ui.Eval(`document.getElementById('content').innerHTML="` + str + `";`)
+}
 
-	t1 := time.Now()
-	//f.search(level, number, c1)
-	f.search(number, c1)
-	Ans1 := <-c1
-	t2 := time.Now().Sub(t1)
-	fmt.Println(Ans1, t2)
+func MCFloat(n int, c chan int) {
+	local_Pi := 0
+	for i := 0; i < n; i++ {
+		x := rand.Float64()
+		X := math.Pow(x, 2)
+		y := rand.Float64()
+		Y := math.Pow(y, 2)
+		if X+Y <= 1 {
+			local_Pi += 1
+		}
+	}
+	c <- local_Pi
+}
 
-	t3 := time.Now()
-	//go f.searchGo(level, number, c2)
-	go f.searchGo(number, c2)
-	Ans2 := <-c2
-	t4 := time.Now().Sub(t3)
-	fmt.Println(Ans2, t4)
+func MCBig(n int, c chan int) {
+	local_Pi := 0
+	X := new(big.Float).SetPrec(1024)
+	Y := new(big.Float).SetPrec(1024)
+	Z := new(big.Float).SetPrec(1024)
+	for i := 0; i < n; i++ {
+		x := rand.Float64()
+		xBig := new(big.Float).SetFloat64(x)
+		X.Mul(xBig, xBig)
+		y := rand.Float64()
+		yBig := new(big.Float).SetFloat64(y)
+		Y.Mul(yBig, yBig)
+		Z.Add(X, Y)
+		cmp := Z.Cmp(big.NewFloat(1))
+		if cmp != 1 {
+			local_Pi += 1
+		}
+	}
+	c <- local_Pi
+}
 
-	ui.Eval(`document.getElementById('content3').innerHTML="` + "Seq : " + t2.String() + "   Go : " + t4.String() + `";`)
+func MCgoFloat(n int, c chan int) {
+	local_Pi := 0
+	for i := 0; i < n; i++ {
+		x := rand.Float64()
+		X := math.Pow(x, 2)
+		y := rand.Float64()
+		Y := math.Pow(y, 2)
+		if X+Y <= 1 {
+			local_Pi += 1
+		}
+	}
+	c <- local_Pi
+	//wg.Done()
+}
+
+func MCgoBig(n int, c chan int) {
+	local_Pi := 0
+	X := new(big.Float).SetPrec(1024)
+	Y := new(big.Float).SetPrec(1024)
+	Z := new(big.Float).SetPrec(1024)
+	for i := 0; i < n; i++ {
+		x := rand.Float64()
+		xBig := new(big.Float).SetFloat64(x)
+		X.Mul(xBig, xBig)
+		y := rand.Float64()
+		yBig := new(big.Float).SetFloat64(y)
+		Y.Mul(yBig, yBig)
+		Z.Add(X, Y)
+		cmp := Z.Cmp(big.NewFloat(1))
+		if cmp != 1 {
+			local_Pi += 1
+		}
+	}
+	c <- local_Pi
+	//wg.Done()
 }
