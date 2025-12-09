0c85389519385d6abd22bc03700d48c4a586d3cd
2220047-Omura <h20047@e.hiroshima-cu.ac.jp>
2025-12-09 18:30:02 +0900

    add mpfi.ver

---
+++ 2025-12-08/LU-cgo-Skyline-mpfi/LU-cgo-Skyline-mpfi.c
@@ -0,0 +1,175 @@
+#include <stdio.h>
+#include <stdlib.h>
+//#include <time.h>
+#include <mpfi.h>
+#include <mpfi_io.h>
+//#include <mpfr.h>
+
+void printInterval(__mpfi_struct *b);
+//void comp(void);
+
+#define size 500
+int n;
+int N;
+int acc = 1024;
+char buf[256];
+
+mpfi_t *Ask;
+int isk[size];
+mpfi_t *Lsk;
+
+mpfi_t *SUMsk;
+mpfi_t *MULsk;
+
+mpfi_t tmp1;
+mpfi_t tmp2;
+//mpfi_t tmp;
+
+int getN(void){
+    return N;
+}
+
+int getIsk(int c){
+    return isk[c];
+}
+
+/*
+void printMatrix(__mpfi_struct *array) {
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < N; j++) {
+            printInterval(ptr(array, i, j));
+        }
+        printf("\n");
+    }
+}
+*/
+
+int init(void) {
+	mpfr_t a;
+	mpfr_init2(a, acc);
+
+    // define N
+    for (int i = 1; i < size; i++) {
+        n -= 1;
+        if (n < 0) {
+            n = i;
+        }
+        N = N + n + 1;
+    }
+    //printf("N:%d\n",N);
+
+    // allocate
+    Ask = (mpfi_t *)malloc(N * sizeof(mpfi_t));
+    Lsk = (mpfi_t *)malloc(N * sizeof(mpfi_t));
+    SUMsk = (mpfi_t *)malloc(N * sizeof(mpfi_t));
+    MULsk = (mpfi_t *)malloc(N * sizeof(mpfi_t));
+    
+    for (int i = 0; i < N; i++) {
+        mpfi_init2(Ask[i], acc);
+        mpfi_init2(Lsk[i], acc);
+        mpfi_init2(SUMsk[i], acc);
+        mpfi_init2(MULsk[i], acc);
+    }
+    //printInterval((__mpfi_struct *)&(Ask[0]));
+    //mpfi_init2(tmp, acc);
+    //mpfi_init2(MUL, acc);
+    mpfi_init2(tmp1, acc);
+    mpfi_init2(tmp2, acc);
+	return 0;
+}
+
+void setSkyline(){
+    mpfr_t a;
+ 	mpfr_init2(a, acc);
+
+    n = 0;
+	isk[0] = 0;
+	for (int i = 1; i < size; i++) {
+		n -= 1;
+		if (n < 0) {
+			n = i;
+		}
+		isk[i] = isk[i-1] + n + 1;
+	}
+    for (int i = 0; i < N; i ++){
+        double r = ((double)rand())/RAND_MAX;
+	    mpfr_set_d(a, r, MPFR_RNDN);
+	    mpfi_interv_fr(Ask[i], a, a);
+    }
+}
+
+void setSkylineTest(){
+    int AskTest[10] = {2, 1, 3, 0, 4, 7, 8, 2, 3, 5};
+	//N = 10;
+	for (int i = 0; i < 10; i++) {
+        mpfi_set_si(Ask[i],AskTest[i]);
+        //printInterval((__mpfi_struct *)&(Ask[i]));
+	}
+	int iskTest[5] = {0, 1, 4, 5, 9};
+	for (int i = 0; i < 5; i++) {
+		isk[i] = iskTest[i];
+        //printf("isk : %d\n",isk[i]);
+	}
+}
+
+void reset(){
+	setSkyline();
+	//setSkylineTest();
+	for (int i = 0; i < N; i++) {
+        mpfi_set_str(Lsk[i], "0", 10);
+        mpfi_set_str(SUMsk[i], "0", 10);
+        mpfi_set_str(MULsk[i], "0", 10);
+	}
+}
+
+void Usetsk(int b,int i,int j){
+    int s;
+    if (isk[j]-isk[j-1]-(j-i)-1 < isk[i]-isk[i-1]-1) {
+		s = isk[j] - isk[j-1] - (j - i) - 1;
+	} else {
+		s = isk[i] - isk[i-1] - 1;
+	}
+
+    for (int k = 0; k < s; k++) {
+        mpfi_div(Lsk[b],Ask[isk[i]-(s-k)],Ask[isk[i-(s-k)]]);
+        mpfi_mul(MULsk[b],Lsk[b],Ask[isk[j]-(j-i)-(s-k)]);
+        mpfi_add(SUMsk[b],SUMsk[b],MULsk[b]);
+    }
+    mpfi_sub(Ask[b], Ask[b], SUMsk[b]);
+    //printInterval((__mpfi_struct *)&(Ask[b]));
+}
+
+void printInterval(__mpfi_struct *b) {
+    char buf[256];
+    mpfr_exp_t exp;
+    mpfr_get_str(buf, &exp, 10, 15,
+        // &((__mpfi_struct *)&(b))->left, MPFR_RNDD);
+        &(b->left), MPFR_RNDD);
+    printf("[%sx(%d), ", buf, (int)exp);
+    mpfr_get_str(buf, &exp, 10, 15,
+        &(b->right), MPFR_RNDU);
+    printf("%sx(%d)]\n", buf, (int)exp);
+}
+
+void result(){
+    for (int i = N-3; i < N; i++) {
+        printInterval((__mpfi_struct *)&(Ask[i]));
+    }
+}
+
+void clear(){
+    //printf("clear");
+    memset(isk,0,sizeof(isk));
+    free(Ask);
+    free(Lsk);
+    free(SUMsk);
+    free(MULsk);
+}
+
+/*
+void printM(__mpfi_struct *b) {
+
+}
+*/
+
+
